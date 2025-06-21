9ea24aa8dd4513aee28152748dca795ac38cd913
arturogood17 <arturo_good17@hotmail.com>
2025-06-21 12:38:48 +0200

    Available functions with getcontent, getfiles, writefile, runpython

--- call_function.py
+++ call_function.py
@@ -1,8 +1,14 @@
 from google.genai import types
 from functions.get_files_info import schema_get_files_info
+from functions.get_content import schema_get_content
+from functions.writefile import schema_write_file
+from functions.run_python import schema_run_python
 
 available_functions= types.Tool(
          function_declarations=[
               schema_get_files_info,
+              schema_write_file,
+              schema_run_python,
+              schema_get_content,
          ]
     )
